0a83a5a6f2739465b6b9e274d077d75c27441d58
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-01-09 15:45:16 -0300

    commit desafios 1

---
+++ src/main/Main.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.util.SortedMap;
+
+import javax.crypto.KEM;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("1) Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0;\r\n"
+				+ "Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }\r\n" + "Imprimir(SOMA);\r\n"
+				+ "Ao final do processamento, qual será o valor da variável SOMA?");
+
+		int indice = 13;
+		int soma = 0;
+		int k = 0;
+
+		while (k < indice) {
+			k = k + 1;
+			soma = soma + k;
+		}
+		
+		System.out.println(soma);
+
+	}
+
+}
